cd5775c3adb12bbc8509ae47b0868bf45ac7f540
Yash Raikar <yashraikar1087@gmail.com>
2022-05-11 12:31:20 +0100

    add location functionality

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -6,12 +6,21 @@ interface IMap {
     mapTypeControl?: boolean
 }
 
+interface IMarker {
+    address: string;
+    latitude: number;
+    longitude: number;
+}
+
 type GoogleLatLng= google.maps.LatLng;
 type GoogleMap= google.maps.Map;
+type GoogleMarker = google.maps.Marker;
 
 const  Map: React.FC<IMap> = ({mapType, mapTypeControl=false}) => {
     const ref = useRef<HTMLDivElement>(null);
     const [map, setMap] = useState<GoogleMap>();
+    const [marker, setMarker] = useState<IMarker>();
+
     const startMap = (): void => {
         if(!map) {
             //TODO
@@ -25,6 +34,59 @@ const  Map: React.FC<IMap> = ({mapType, mapTypeControl=false}) => {
         //TODO: initmap
         initMap(5, defaultAddress);
     }
+
+    const initEventListener = (): void => {
+        if(map) {
+            google.maps.event.addListener(map, 'click', function(e){
+                coordinateToAddress(e.latLng)
+            });
+        }
+
+    }
+    useEffect(initEventListener, [map]);
+
+    const coordinateToAddress = async (coordinate:GoogleLatLng) => {
+        const geocoder = new google.maps.Geocoder();
+        console.log(coordinate)
+        await geocoder.geocode({location: coordinate}, function(results, status) {
+            if(status === 'OK') {
+                console.log(results)
+                setMarker({
+                    address: results[0].formatted_address,
+                    longitude: coordinate.lat(),
+                    latitude: coordinate.lng()
+                })
+            }
+        })
+    };
+
+    const addSingleMarker = ():void => {
+        console.log("in add single marker")
+        console.log(marker)
+        if(marker) {
+            addMarker(new google.maps.LatLng(marker.latitude, marker.longitude));
+        }
+
+    };
+
+    useEffect(addSingleMarker, [marker]);
+
+    const addMarker = (location:GoogleLatLng):void => {
+        const marker: GoogleMarker =new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: getIconAttributes('#000000')
+        });
+    };
+
+    const getIconAttributes = (iconColor: string) => {
+        return  {
+            path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+            scale: 10
+        }
+    }
+
+
 
     const initMap = (zoomLevel: number, address: GoogleLatLng): void => {
         if(ref.current) {
